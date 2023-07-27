67e3f10f12be1a69336e0bf78827fed2c78c2ad9
Alexis <alexis.prunier@securitymadein.lu>
2023-07-27 12:23:12 +0200

    Disable chip links

--- src/component/form/Chip.jsx
+++ src/component/form/Chip.jsx
@@ -10,6 +10,13 @@ export default class Chip extends Component {
 	}
 
 	render() {
+		/*
+			this.props.url has been replaced by this.props.url_fake
+
+			The usage of the links on the Chips has not been implemented.
+			Mostly used for the search page redirection, it could be
+			set on demand.
+		*/
 		return <div className="Chip">
 			<div
 				className="Chip-head"
@@ -17,17 +24,22 @@ export default class Chip extends Component {
 				<i className="fas fa-hashtag"/>
 			</div>
 
-			<object
-				className={"Chip-link-wrapper"}
-				onClick={() => {
-					if (this.props.url) {
-						window.open(this.props.url, "_self");
-					}
-				}}>
-				<div className="Chip-content">
+			{this.props.url_fake
+				? <object
+					className={"Chip-link-wrapper"}
+					onClick={() => {
+						if (this.props.url_fake) {
+							window.open(this.props.url_fake, "_self");
+						}
+					}}>
+					<div className="Chip-content">
+						{this.props.label}
+					</div>
+				</object>
+				: <div className="Chip-content">
 					{this.props.label}
 				</div>
-			</object>
+			}
 
 			{this.props.onClick !== undefined
 				? <div className="Chip-close">
